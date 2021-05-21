4a6bc1ad36b925cc31ee26aaeefc02a735771e86
Your Name <mikaylabeelders@gmail.com>
2021-05-21 11:59:55 +0200

    new changes

--- main.py
+++ main.py
@@ -1,90 +1,109 @@
 # Mikayla Beelders
 from tkinter import *
-# defining the functions
-# calculating the BMI
-def calc_BMI():
-    calc_BMI = (int(entry_w.get()))/(int(entry_h.get()/100)**2)
-    label_bmi.set(calc_BMI)
-# calculating the Ideal BMI
-def calc_IdealBMI():
-    weight_value= entry_w.get()
-    height_value= entry_h.get()
-    calc_IdealBMI = (0.5*(int(weight_value))/(int(height_value/100)**2)+11.5)
-    label_ibmi.set(calc_IdealBMI)
-# clearing the numbers
-def clear_numbers():
-    entry_w.delete(0, END)
-    entry_h.delete(0, END)
-    entry_a.delete(0, END)
-# exiting the program
-def exit_program():
-    return root.destroy()
+from tkinter import messagebox
 
-root=Tk()
-# var=IntVar
-label_text=StringVar
-root.geometry ("600x600")
-root.title("Ideal Body Mass Index")
-Text="Ideal Body Mass Index Calculator"
-frame_1 = Frame(root, width=10, height=2, highlightbackground = 'black', highlightthickness = 2)
-frame_1.pack(fill="both", expand="yes")
-heading = Label(frame_1, text="Ideal Body Mass Index Calculator")
-heading.pack()
-text = Label(frame_1, text="Enter your weight, height and gender")
-text.pack()
-# weight label and entry as well as their places
-label_w= Label(frame_1, text= "Weight: ")
-label_w.place(x=20, y=80)
-entry_w= Entry(frame_1)
-entry_w.place(x=80, y=80)
-label_kg = Label(frame_1, text="Kilograms")
-label_kg.place(x=250, y=80)
+#  Get height value from entry
+def get_height():
+    height = float(entry_h.get())
+    return round(height)
 
-# height label and entry as well as their places
-label_h= Label(frame_1, text= "Height: ")
-label_h.place(x=20, y=120)
-entry_h= Entry(frame_1)
-entry_h.place(x=80, y=120)
-label_cm = Label(frame_1, text="cm")
-label_cm.place(x=250, y=120)
-# age label and entry as well as their places
-label_a= Label(frame_1, text= "Age: ")
-label_a.place(x=360, y=120)
-entry_a= Entry(frame_1)
-entry_a.place(x=400, y=120)
-# gender label and selector
-label_g= Label(frame_1, text= "Gender: ")
-label_g.place(x=20, y=160)
-genders = [
-    "Select",
-    "Male",
-    "Female",
-]
-clicked= StringVar()
-clicked.set(genders[0])
+# Get weight from entry
+def get_weight():
+    weight = float(entry_w.get())
+    return round(weight)
 
-drop = OptionMenu(root, clicked, *genders)
-drop.pack(pady=20)
-drop.place(x = 80, y=160)
-#BMI label_
-label_bmi= Label(root, text= "BMI: ")
-label_bmi.place(x=20, y=350)
-entry_bmi= Entry(root)
-entry_bmi.place(x=80, y=350)
-#IBMI label
-label_ibmi= Label(root, text= "Ideal BMI: ")
-label_ibmi.place(x=330, y=350)
-entry_ibmi= Entry(root)
-entry_ibmi.place(x=400, y=350)
-# The necessary buttons
-# Calculating BMI
-calc_button = Button (root, text="Calculate your Ideal Body Mass Index", command=calc_BMI)
-calc_button.place(x=150, y=200)
-# Clear numbers
-clear_button = Button (root, text="Clear", command=clear_numbers)
-clear_button.place(x=20, y=540)
-# Exit program
-exit_button = Button (root, text="Exit", command=exit_program)
-exit_button.place(x=520, y=540)
+def calculate_bmi(b=""):   # Because the bind function gives an argument to the function, I have put b there
+    print(b)
+# In order to calculate the result
+    try:
+        height = get_height()
+        weight = get_weight()
+        height = height / 100.0
+        bmi = round(weight / (height ** 2))
+    except ZeroDivisionError:
+        messagebox.showinfo("Result", "Please enter a positive height:")
+    except ValueError:
+        messagebox.showinfo("Result", "Please enter valid inputs:")
+    else:
+        if bmi <= 15.0:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Very severely underweight!!"
+            messagebox.showinfo("Result", res)
+        elif 15.0 < bmi <= 16.0:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Severely underweight!"
+            messagebox.showinfo("Result", res)
+        elif 16.0 < bmi < 18.5:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Underweight!"
+            messagebox.showinfo("Result", res)
+        elif 18.5 <= bmi <= 25.0:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Normal."
+            messagebox.showinfo("Result", res)
+        elif 25.0 < bmi <= 30:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Overweight."
+            messagebox.showinfo("Result", res)
+        elif 30.0 < bmi <= 35.0:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Moderately obese!"
+            messagebox.showinfo("Result", res)
+        elif 35.0 < bmi <= 40.0:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Severely obese!"
+            messagebox.showinfo("Result", res)
+        else:
+            res = "Your BMI is " + str(bmi) + "\nRemarks: Super obese!!"
+            messagebox.showinfo("Result", res)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Body Mass Index Calculator")
+    root.bind("<Return>", calculate_bmi)
+    root.geometry("700x700")
+    root.configure(bg="#9900ff")
+    root.resizable(width=False, height=False)
+
+    frame = Frame(root, width=590, height=430, relief='raised', bg='#b84dff')
+    frame.place(relx=0.08, rely=0.1)
+    # heading label
+    label_heading = Label(root, bg="#b84dff",fg ="white", text="IDEAL BODY MASS INDEX CALCULATOR", font=("Helvetica", 15, "bold"), pady=5)
+    label_heading.place(relx=0.2, rely=0)
+
+    # weight entry and label
+    label_w = Label(root, bg="#b84dff",fg ="white", text="Enter Weight (in kg):", bd=6,
+                   font=("Helvetica", 10, "bold"), pady=5)
+    label_w.place(x=155, y=160)
+    entry_w = Entry(root, bg="#9900ff",fg ="white", bd=8, width=6, font="Helvetica")
+    entry_w.place(x=370, y=160)
+
+    # height entry and label
+    label_h = Label(root, bg="#b84dff",fg ="white", text="Enter Height (in cm):", bd=6,
+                   font=("Helvetica", 10, "bold"), pady=5)
+    label_h.place(x=155, y=121)
+    entry_h = Entry(root,bg="#9900ff",fg ="white",  bd=8, width=6, font="Helvetica")
+    entry_h.place(x=370, y=121)
+
+    # BMI button
+    BUTTON = Button(bg="#9900ff", fg ="white",bd=12, text="BMI", padx=33, pady=15, command=calculate_bmi,
+                    font=("Helvetica", 20, "bold"))
+    BUTTON.place(x=250, y=350)
+
+gender =Label(frame, text="Gender:", bg='#b84dff',fg ="white")
+gender.place(relx=0.18, rely=0.35)
+
+age = Label(frame, text="Age:", bg='#b84dff', fg='white')
+age.place(relx=0.18, rely=0.45)
+age_entry = Entry(frame, bg="#9900ff", state='readonly')
+age_entry.place(relx=0.35, rely=0.45)
+
+options = ['Select...', 'Male', "Female"]
+variable = StringVar(frame)
+variable.set(options[0])
+
+
+def activate(value):
+    variable.set(value)
+    if value != "Select...":
+        age_entry.config(state='normal', bg="#b84dff")
+    else:
+        age_entry.config(state='readonly')
+
+gender_menu = OptionMenu(frame, variable, *options, command=activate)
+gender_menu.place(relx=0.35, rely=0.35)
 
 root.mainloop()
